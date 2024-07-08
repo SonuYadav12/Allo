30ee663384db12ed18b2344772083a247e9f11c9
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-07-09 00:26:56 +0530

    Category Component Implemented

--- src/Component/Category/filter.js
+++ src/Component/Category/filter.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import food from '../../asset/data.json';
 
-const filter = () => {
-  return (
-    <div>filter</div>
-  )
-}
+const FilterBox = () => {
+    const [items, setItems] = useState(food.meals);
+    const [toggle1, setToggle1] = useState('all');
 
-export default filter
+    const toggleBTN = (index, id) => {
+        const updatedItems = food.meals.filter((item) => item.labels.includes(index));
+        setItems(updatedItems);
+        setToggle1(id);
+    };
+
+    return (
+        <section className='flex flex-col md:flex-row'>
+            <section className="dashboard">
+                <div className='flex flex-wrap bg-secondary p-10 mt-8 mb-4 rounded-t-2xl'>
+                    <div
+                        className={`border-2 border-light-color py-2.5 px-14 rounded-full m-4 cursor-pointer hover:bg-primary ${toggle1 === 'all' ? "border-tertiary bg-light-color text-tertiary" : ""}`}
+                        onClick={() => {
+                            setItems(food.meals);
+                            setToggle1('all');
+                        }}
+                    >
+                        <p className="filters"> All </p>
+                    </div>
+                    {
+                        food.labels.map((data) => (
+                            <div
+                                className={`border-2 border-light-color py-2.5 px-14 rounded-full m-4 cursor-pointer hover:bg-primary ${toggle1 === data.id ? "border-tertiary bg-light-color text-tertiary" : ""}`}
+                                key={data.id}
+                                onClick={() => toggleBTN(data.label.toLowerCase(), data.id)}
+                            >
+                                <p className="filterName"> {data.label} </p>
+                            </div>
+                        ))
+                    }
+                </div>
+               
+            </section>
+        </section>
+    );
+};
+
+export default FilterBox;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: 'var(--primary)',
+                secondary: 'var(--secondary)',
+                tertiary: 'var(--tertiary)',
+                'light-color': 'var(--light-color)',
+            },
+            borderWidth: {
+                '3': '3px',
+            },
+        },
+    },
+    plugins: [],
 }
